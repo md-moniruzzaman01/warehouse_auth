f4ccf0fc71d9feea31f3203084812a8e2f8f206c
Md Moniruzzaman <md.moniruzzaman1050@gmail.com>
2024-10-15 16:53:12 +0600

    setup packages

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+
+
+const app: Application = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req: Request, res: Response) => {
+//   throw new Error('Testing Error logger')
+res.send("Testing");
+})
+
+
+//global error handler
+// app.use(globalErrorHandler);
+
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+      success: false,
+      message: 'Not Found',
+      errorMessages: [
+        {
+          path: req.originalUrl,
+          message: 'API Not Found',
+        },
+      ],
+    });
+    next();
+  });
+  
+  export default app;
