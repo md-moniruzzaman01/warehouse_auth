9b7152c1a350bf0cbfd8ee62893364b4077d9c13
Md Moniruzzaman <md.moniruzzaman1050@gmail.com>
2024-10-16 16:08:54 +0600

    add pagination,errorloger,gobleError handler, interface, config file and install zod and winston

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, NextFunction, Request, Response } from "express";
-import cookieParser from "cookie-parser";
-const app: Application = express();
-app.use(cors());
 
+const app: Application = express();
+
+app.use(cors());
 app.use(cookieParser());
+
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req: Request, res: Response) => {
+  //   throw new Error('Testing Error logger')
+  res.send("Testing");
+});
+
+//global error handler
+// app.use(globalErrorHandler);
+
+//handle not found
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(404).json({
     success: false,
